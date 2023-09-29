ea429454273a71bb704683358c7412d561716ef0
chamath <csschamathinduwara@gmail.com>
2023-09-29 12:57:18 +0530

    crate New.txt component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Destinations from "@/components/destinations/Destinations";
+import New from "@/components/new/New";
 
 export default function Home() {
   return (
     <main className="flex flex-col">
       <Destinations />
+      <New/>
     </main>
   );
 }
--- src/components/new/New.tsx
+++ src/components/new/New.tsx
@@ -1,7 +1,13 @@
 "use client";
 
 const New = () => {
-  return <div>New</div>;
+  return (
+    <div>
+      <h1>NEW</h1>
+      <p>adslf asd jfa lkasjdf jdsa</p>
+      <div></div>
+    </div>
+  );
 };
 
 export default New;
